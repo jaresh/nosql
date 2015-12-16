9f372b75ea7ee1389460e28fba71c1b6cf10fec1
jaresh <jaresh@wp.pl>
2015-12-16 19:21:33 +0100

    aggregation1.py mod

--- scripts/aggregation1.py
+++ scripts/aggregation1.py
@@ -8,7 +8,7 @@ collection = db.list
 
 pipeline = [
 	{ "$match": {"Month": "2012-06"} }, 
-    { "$group": {"_id": "$Crime type", "total": {"$sum": 1}} }, 
+	{ "$group": {"_id": "$Crime type", "total": {"$sum": 1}} }, 
   	{ "$sort": SON([("total", -1)])},
 	{ "$limit": 5 }
 ]
